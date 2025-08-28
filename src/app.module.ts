a8d642a5491be459d98b7d2cab89e5c1390c841b
Jorge Reyes <jorgeart81dev@gmail.com>
2025-08-28 13:18:50 -0500

    install prisma, add . env and docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { SampleModule } from './sample/sample.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
