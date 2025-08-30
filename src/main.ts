09a9c43e886849abdae487dd681a178f6e026a2a
Jorge Reyes <jorgeart81dev@gmail.com>
2025-08-29 21:37:11 -0500

    feat: enableCors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.FRONTEND_ORIGIN,
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
